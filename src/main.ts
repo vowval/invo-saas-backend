3932285521fa54d9a26c69ff875e4b27167ef372
vowval <jag.jagannath@gmail.com>
2026-09-09 20:52:39 +0530

    main chnage

--- src/main.ts
+++ src/main.ts
@@ -24,11 +24,18 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], // frontend
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'https://invo-saas-frontend.vercel.app',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
 
-  await app.listen(3001);
+  const port = process.env.PORT || 3001;
+
+  await app.listen(port);
 }
 bootstrap();
